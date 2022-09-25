b7dfae016fb04c7e4a32a86b0ec1538b3420e8fb
Ikoyo Daniel Omovigho <dannirolands@gmail.com>
2022-09-25 21:35:35 +0100

    removed banner.js

--- pages/index.js
+++ pages/index.js
@@ -11,12 +11,16 @@ import SEO from "../components/SEO";
 export default function Home() {
   const { userId, setUserId } = useUserContext();
 
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, [userId]);
+
   if (userId) {
     return (
       <Layout>
         <SEO />
         <AddInTop />
-        <Banner href="https://song.cooo.me/select?ref=secret" />
+        {/* <Banner href="https://song.cooo.me/select?ref=secret" /> */}
 
         <div className="w-full max-w-3xl mx-auto px-3 p-2 mt-3 bg-white border border-gray-600 rounded-xlg">
           <SharableView userId={userId} />
@@ -36,7 +40,7 @@ export default function Home() {
           <div className="text-center bg-gray-200 h-full min-h-screen">
             <SEO />
             <AddInTop />
-            <Banner href="https://song.cooo.me/select?ref=secret" />
+            {/* <Banner href="https://song.cooo.me/select?ref=secret" /> */}
 
             <div className="w-full max-w-3xl mx-auto px-3 p-2 mt-3 bg-white border border-gray-600 rounded-xlg">
               <StartView onUserSaved={() => setUserId("1234")} />
